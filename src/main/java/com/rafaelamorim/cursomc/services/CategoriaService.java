0d7bffe4c5f0de2e1cf2c8f44df5ca73bcf0a76a
Rafael Amorim <rafaelamorimclash@gmail.com>
2018-12-20 15:27:09 -0200

    Listando categorias com DTO

--- src/main/java/com/rafaelamorim/cursomc/services/CategoriaService.java
+++ src/main/java/com/rafaelamorim/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.rafaelamorim.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,6 +42,9 @@ public class CategoriaService {
 		}catch(DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possivel excluir categoria que contém produtos");
 		}
-		
+	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 }
